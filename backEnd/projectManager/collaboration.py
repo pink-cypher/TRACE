7a686d28481084169b78db8c9584c3a2b7a63232
Raulpal <rpallares@miners.utep.edu>
2025-04-03 21:22:56 -0600

    added all collaboration function need to invite, remove, list collaborators on a project

---
+++ backEnd/projectManager/collaboration.py
@@ -0,0 +1,14 @@
+from databasemanager.databasemanager import db
+
+class CollaborationManager:
+    def __init__(self):
+        pass
+
+    def invite_to_project(self, lead_initials, analyst_initials, projectID):
+        return db.add_Collaborator(lead_initials, analyst_initials, projectID)
+
+    def list_collaborators(self, projectID):
+        return db.getCollaborators(projectID)
+
+    def remove_collaborator(self, lead_initials, analyst_initials, projectID):
+        return db.removeCollaborator(lead_initials, analyst_initials, projectID)
